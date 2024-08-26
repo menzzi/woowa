25605f97704598e9e00f29da305184c6c537bee2
menzzi <minmin3927@naver.com>
2024-08-26 09:56:49 +0900

    feat: split,calculateMaxAddOrMultiply 함수 구현

--- java-onboarding/src/main/java/onboarding/Problem1.java
+++ java-onboarding/src/main/java/onboarding/Problem1.java
@@ -17,12 +17,50 @@ class Problem1 {
 
         return true;
     }
+    public static int[] split(int num){
+        int third = num / 100;
+        int second = (num % 100)/10;
+        int first = num % 10;
+
+        int[] splitNum = {third,second,first};
+        return splitNum;
+    }
+
+    public static int calculateMaxAddOrMultiply(List<Integer> pages){
+        int max = 0;
+        int[] left = split(pages.get(0));
+        int[] right = split(pages.get(1));
+
+        int add = Math.max(left[0] + left[1] + left[2],right[0] + right[1] + right[2]);
+        if(left[0]==0){
+            left[0] = 1;
+        }
+        if(right[0]==0){
+            right[0]=1;
+        }
+        int multiply = Math.max(left[0]*left[1]*left[2],right[0]*right[1]*right[2]);
+
+        max = Math.max(add,multiply);
+
+        return max;
+    }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+
         if(!checkException(pobi)||!checkException(crong)){
             return -1;
         }
-        return answer;
+
+        int maxPobi = calculateMaxAddOrMultiply(pobi);
+        int maxCrong = calculateMaxAddOrMultiply(crong);
+
+        if(maxPobi > maxCrong){
+            return 1;
+        }else if(maxPobi == maxCrong){
+            return 0;
+        }else{
+            return 2;
+        }
+
     }
 }
