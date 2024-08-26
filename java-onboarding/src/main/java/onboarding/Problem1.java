112beae7734e1288cadec85af9fc3fb097ab1124
menzzi <minmin3927@naver.com>
2024-08-26 09:23:15 +0900

    feat: checkException 함수 구현

--- java-onboarding/src/main/java/onboarding/Problem1.java
+++ java-onboarding/src/main/java/onboarding/Problem1.java
@@ -3,8 +3,26 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    public static boolean checkException(List<Integer> pages){
+        int left = pages.get(0);
+        int right = pages.get(1);
+
+        if(left != right -1){
+            return false;
+        }
+        if(left < 1 || right > 400){
+            return false;
+        }
+
+        return true;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if(!checkException(pobi)||!checkException(crong)){
+            return -1;
+        }
         return answer;
     }
 }
