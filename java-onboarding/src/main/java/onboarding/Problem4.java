7ad42b09c49baeca5c8071c169324ee6f020b883
menzzi <minmin3927@naver.com>
2024-08-28 10:40:09 +0900

    [4번 문제] refactor: checkUpOrLowg 함수 수정(좌우 구분 -> 대소문자 구분)

--- java-onboarding/src/main/java/onboarding/Problem4.java
+++ java-onboarding/src/main/java/onboarding/Problem4.java
@@ -2,22 +2,33 @@ package onboarding;
 
 public class Problem4 {
 
-    public static int checkLeftOrRight(char c){
+    public static int checkUpOrLow(char c){
         int asciiChar = (int)c;
-        if((asciiChar >= 65 && asciiChar <= 77) || (asciiChar >= 97 && asciiChar <= 109)){
+        if(asciiChar >= 65 && asciiChar <= 90){
             return -1;
-        }else if((asciiChar > 77 && asciiChar <= 90) || (asciiChar > 109 && asciiChar <= 122)){
+        }else if(asciiChar >= 97 && asciiChar <= 122){
             return 1;
         }
 
         return 0;
+    }
+    public static char exchangeUpper(char c){
+        if()
+    }
+    public static char exchangeLower(char c){
+
     }
 
     public static String solution(String word) {
         StringBuilder answer = new StringBuilder();
 
         for(int i=0;i<word.length();i++){
-            checkLeftOrRight(word.charAt(i));
+            char ch = word.charAt(i);
+            if(checkUpOrLow(ch) == -1){
+                answer.append(exchangeUpper(ch));
+            }else if(checkUpOrLow(ch) == 1){
+                answer.append(exchangeLower(ch));
+            }
         }
 
         return answer.toString();
