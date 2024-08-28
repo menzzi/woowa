4e2c0d3cde2a48dc7af3165ca6c5c0b9c23766dc
menzzi <minmin3927@naver.com>
2024-08-28 10:27:43 +0900

    [4번 문제] feat: checkLeftOrRight 함수 구현

--- java-onboarding/src/main/java/onboarding/Problem4.java
+++ java-onboarding/src/main/java/onboarding/Problem4.java
@@ -1,8 +1,25 @@
 package onboarding;
 
 public class Problem4 {
+
+    public static int checkLeftOrRight(char c){
+        int asciiChar = (int)c;
+        if((asciiChar >= 65 && asciiChar <= 77) || (asciiChar >= 97 && asciiChar <= 109)){
+            return -1;
+        }else if((asciiChar > 77 && asciiChar <= 90) || (asciiChar > 109 && asciiChar <= 122)){
+            return 1;
+        }
+
+        return 0;
+    }
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+        for(int i=0;i<word.length();i++){
+            checkLeftOrRight(word.charAt(i));
+        }
+
+        return answer.toString();
     }
 }
