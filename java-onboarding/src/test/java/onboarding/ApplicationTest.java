42e52918ccec696c6808700e3162c722c21a8c72
menzzi <minmin3927@naver.com>
2024-08-28 10:45:28 +0900

    [1번 문제] test: 테스트케이스 추

--- java-onboarding/src/test/java/onboarding/ApplicationTest.java
+++ java-onboarding/src/test/java/onboarding/ApplicationTest.java
@@ -41,6 +41,14 @@ class ApplicationTest {
             int result = -1;
             assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
         }
+
+        @Test
+        void case5(){
+            List<Integer> pobi = List.of(99, 100);
+            List<Integer> crong = List.of(401, 402);
+            int result = -1;
+            assertThat(Problem1.solution(pobi, crong)).isEqualTo(result);
+        }
     }
 
     @Nested
