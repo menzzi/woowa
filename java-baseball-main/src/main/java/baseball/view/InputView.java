1582a68f236e5b10e31bff6a576c1882a8103a13
menzzi <minmin3927@naver.com>
2024-09-01 17:39:10 +0900

    refactor: InputView 수정

--- java-baseball-main/src/main/java/baseball/view/InputView.java
+++ java-baseball-main/src/main/java/baseball/view/InputView.java
@@ -1,15 +1,12 @@
 package baseball.view;
 
-import camp.nextstep.edu.missionutils.Console;
-
 import java.util.Scanner;
 
 public class InputView {
-    public static String getUserInput(String message){
-        Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
+    public static String getUserInput(String message){
         System.out.printf(message);
-        String userInput = scanner.nextLine();
-        return userInput;
+        return scanner.nextLine();
     }
 }
