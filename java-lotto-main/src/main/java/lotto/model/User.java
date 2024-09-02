ae9105ef5e1761ebab9e5fd436d56f62131f7efa
menzzi <minmin3927@naver.com>
2024-09-02 17:49:40 +0900

    feat : calculateAmount 메서드 구현

--- java-lotto-main/src/main/java/lotto/model/User.java
+++ java-lotto-main/src/main/java/lotto/model/User.java
@@ -3,5 +3,12 @@ package lotto.model;
 import lotto.exception.Exception;
 
 public class User {
-
+    public static int calculateAmount(String userAmount){
+        int amount = Integer.parseInt(userAmount);
+        if(amount % 1000 != 0){
+            Exception.moneyRangeException();
+            throw new IllegalArgumentException();
+        }
+        return amount / 1000;
+    }
 }
