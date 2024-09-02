c76204035fa66a24c5b4e916fdd6fe8a5ca815ad
menzzi <minmin3927@naver.com>
2024-09-02 17:30:15 +0900

    feat : Lotto 메서드 구현

---
+++ java-lotto-main/src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,41 @@
+package lotto.model;
+
+import lotto.exception.Exception;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        validateDuplication(numbers);
+        validateRange(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            Exception.countException();
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validateDuplication(List<Integer> numbers){
+        HashSet<Integer> duplicateCheck = new HashSet<>(numbers);
+        if(duplicateCheck.size()!=6){
+            Exception.duplicateException();
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validateRange(List<Integer> numbers){
+        for (Integer number : numbers) {
+            if (number > 45 || number < 1) {
+                Exception.rangeException();
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+}
