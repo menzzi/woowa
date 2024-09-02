b03967a67ecafcb868abb8563ff799af9dceca5c
menzzi <minmin3927@naver.com>
2024-09-02 17:58:05 +0900

    refactor : calculateAmount 메서드 수정

--- java-lotto-main/src/main/java/lotto/controller/LottoController.java
+++ java-lotto-main/src/main/java/lotto/controller/LottoController.java
@@ -1,11 +1,17 @@
 package lotto.controller;
 
+import lotto.model.User;
+import lotto.view.InputView;
+
 public class LottoController {
 
     public void run(){
 
     }
     public void start(){
+        String userAmount = InputView.inputUserAmount();
+        int countLottoTicket = User.calculateAmount(userAmount);
+
 
     }
 }
--- java-lotto-main/src/main/java/lotto/model/User.java
+++ java-lotto-main/src/main/java/lotto/model/User.java
@@ -4,11 +4,24 @@ import lotto.exception.Exception;
 
 public class User {
     public static int calculateAmount(String userAmount){
-        int amount = Integer.parseInt(userAmount);
+        int amount = validateNumber(userAmount);
+        validateRange(amount);
+        return amount / 1000;
+    }
+
+    private static int validateNumber(String userInput){
+        try{
+            return Integer.parseInt(userInput);
+        }catch (NumberFormatException e){
+            Exception.typeException();
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateRange(int amount){
         if(amount % 1000 != 0){
             Exception.moneyRangeException();
             throw new IllegalArgumentException();
         }
-        return amount / 1000;
     }
 }
