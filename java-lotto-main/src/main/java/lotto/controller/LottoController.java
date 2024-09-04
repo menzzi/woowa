b1c0aeca92757854a58653b99d5230c95e3363e7
menzzi <minmin3927@naver.com>
2024-09-04 14:56:08 +0900

    feat(view) : convertStringToList 메서드 구현

--- java-lotto-main/src/main/java/lotto/controller/LottoController.java
+++ java-lotto-main/src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,7 @@
 package lotto.controller;
 
+import lotto.model.Lotto;
+import lotto.model.Result;
 import lotto.model.UserAmount;
 import lotto.model.UserLottos;
 import lotto.view.InputView;
@@ -11,13 +13,21 @@ public class LottoController {
 
     }
     public void start(){
-        String userAmount = InputView.inputUserAmount();
-        int countLottoTicket = UserAmount.calculateAmount(userAmount);
+        int countLottoTicket = inputUserAmount();
         List<List<Integer>> userLottos = UserLottos.generateUserLottos(countLottoTicket);
-
 
 
     }
 
+    public int inputUserAmount(){
+        String userAmount = InputView.inputUserAmount();
+        return UserAmount.calculateAmount(userAmount);
+    }
+    public Result compareLottos(List<List<Integer>> lottoList){
+        Lotto lotto = new Lotto(InputView.inputLottoNumber());
+        for(int i=0;i<lottoList.size();i++){
+
+        }
+    }
 
 }
--- java-lotto-main/src/main/java/lotto/view/InputView.java
+++ java-lotto-main/src/main/java/lotto/view/InputView.java
@@ -2,19 +2,30 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputView {
     public static String inputUserAmount(){
         System.out.println("구입 금액을 입력하세요.");
         return Console.readLine();
     }
 
-    public static String inputLottoNumber(){
+    public static List<Integer> inputLottoNumber(){
         System.out.println("당첨 번호를 입력하세요.");
-        return Console.readLine();
+        return convertStringToList(Console.readLine());
     }
 
     public static String inputBonusNumber(){
         System.out.println("보너스 번호를 입력하세요.");
         return Console.readLine();
+    }
+
+    private static List<Integer> convertStringToList(String input){
+        List<Integer> lottoNumber = new ArrayList<>();
+        for(char c : input.toCharArray()){
+            lottoNumber.add(Character.getNumericValue(c));
+        }
+        return lottoNumber;
     }
 }
