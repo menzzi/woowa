2bc739d9f2f4d3de1e491832618f4019125c2a3a
menzzi <minmin3927@naver.com>
2024-09-05 16:20:28 +0900

    feat(view) : printRankMessage, printRate 메서드 구현

--- java-lotto-main/src/main/java/lotto/view/OutputView.java
+++ java-lotto-main/src/main/java/lotto/view/OutputView.java
@@ -6,8 +6,17 @@ public class OutputView {
         System.out.println(count + "개를 구매했습니다.");
     }
 
+    public static void printRankMessage(){
+        System.out.println("당첨 통계");
+        System.out.println("---");
+    }
+
     public static void printRank(String message, int countMatch){
         System.out.println(message + countMatch + "개");
+    }
+
+    public static void printRate(int rate){
+        System.out.println("총 수익률은" + rate + "%입니다.");
     }
 
 }
