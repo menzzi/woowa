19dd7f56ee5eedd66d4f95ab774266a93ac5cc60
menzzi <minmin3927@naver.com>
2024-09-08 20:58:21 +0900

    test : ValidatorTest 테스트 구현

--- java-bridge-main/src/test/java/bridge/ValidatorTest.java
+++ java-bridge-main/src/test/java/bridge/ValidatorTest.java
@@ -9,4 +9,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ValidatorTest {
 
+    @Test
+    @DisplayName("정상 입력: 3 이상의 숫자")
+    void readBridgeSize_validInput() {
+        final int input = 4;
+
+        assertDoesNotThrow(() -> {
+            Validator.validateBridgeSize(input);
+        });
+    }
+
+    @Test
+    @DisplayName("범위를 벗어난 입력")
+    void readBridgeSize_validInput2() {
+        final int input = 21;
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            Validator.validateBridgeSize(input);
+        });
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 문자 입력")
+    void readBridgeSize_validInput3() {
+        String input = "U\n";
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            Validator.validateNumericInput(input);
+        });
+    }
+
 }
