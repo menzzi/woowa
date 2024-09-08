01af5b44361e7986e8bc4e918e29d3ac9dd92c82
menzzi <minmin3927@naver.com>
2024-09-08 20:38:09 +0900

    feat : validator 구현

---
+++ java-bridge-main/src/main/java/bridge/validator/Validator.java
@@ -0,0 +1,27 @@
+package bridge.validator;
+
+public class Validator {
+    public static void validateBridgeSize(int size){
+        if(size < 3 || size > 20){
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+    }
+    public static void validateNumericInput(String inputSize){
+        try {
+            Integer.parseInt(inputSize);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 입력값은 숫자여야 합니다.");
+        }
+    }
+    public static void validateDirection(String direction){
+        if(!direction.equals("U")||!direction.equals("D")){
+            throw new IllegalArgumentException("[ERROR] 올바른 방향을 입력해주세요.");
+        }
+    }
+    public static void validateRetryOrQuit(String retryOrQuit){
+        if(!retryOrQuit.equals("R")||!retryOrQuit.equals("Q")){
+            throw new IllegalArgumentException("[ERROR] 재시도 : R, 종료 : Q 를 입력하세요.");
+        }
+    }
+
+}
