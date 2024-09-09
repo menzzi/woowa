06d8b37262bad1a4ed95776b04fb0f4046496e8a
menzzi <minmin3927@naver.com>
2024-09-09 19:03:38 +0900

    feat(controller) : startGame 구현 (BridgeGame 클래스 구현하고 마저 구현해야함)

--- java-bridge-main/src/main/java/bridge/controller/BridgeGameController.java
+++ java-bridge-main/src/main/java/bridge/controller/BridgeGameController.java
@@ -1,4 +1,42 @@
 package bridge.controller;
 
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
+
+import java.util.List;
+
+import static bridge.view.InputView.readBridgeSize;
+
 public class BridgeGameController {
+    public void run() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        do{
+            startGame();
+        }while(isProgress());
+    }
+    public void startGame(){
+        int size = inputSize();
+        List<String> answerBridge = makeAnswerBridge(size);
+
+    }
+    public int inputSize(){
+        int size;
+        try{
+            size = readBridgeSize();
+        }catch(IllegalArgumentException e){
+            return inputSize();
+        }
+        return size;
+    }
+
+    public List<String> makeAnswerBridge(int size){
+        BridgeNumberGenerator generator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(generator);
+        return bridgeMaker.makeBridge(size);
+    }
+
+    public boolean isProgress(){
+        return false;
+    }
 }
