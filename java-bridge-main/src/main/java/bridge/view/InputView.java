beec7a64c766467e0d1b264c3298bd30879ce894
menzzi <minmin3927@naver.com>
2024-09-09 16:21:46 +0900

    feat(view) : InputView 구현

--- java-bridge-main/src/main/java/bridge/view/InputView.java
+++ java-bridge-main/src/main/java/bridge/view/InputView.java
@@ -2,8 +2,7 @@ package bridge.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import static bridge.validator.Validator.validateBridgeSize;
-import static bridge.validator.Validator.validateNumericInput;
+import static bridge.validator.Validator.*;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -26,13 +25,19 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String inputDirection = Console.readLine();
+        validateDirection(inputDirection);
+        return inputDirection;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String input = Console.readLine();
+        validateDirection(input);
+        return input;
     }
 }
