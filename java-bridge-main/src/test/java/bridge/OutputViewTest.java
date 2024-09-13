483facc5cde22773d7d699c73cbe91de0ac83950
menzzi <minmin3927@naver.com>
2024-09-13 15:34:35 +0900

    test : OutputViewTest 구현

--- java-bridge-main/src/test/java/bridge/OutputViewTest.java
+++ java-bridge-main/src/test/java/bridge/OutputViewTest.java
@@ -1,10 +1,62 @@
 package bridge;
 
+import bridge.domain.Bridge;
+import bridge.view.OutputView;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class OutputViewTest {
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private OutputView outputView;
+    private Bridge userBridge;
 
+    @BeforeEach
+    public void setUp() {
+        // System.out 출력 캡처
+        System.setOut(new PrintStream(outputStreamCaptor));
+
+        // 테스트할 OutputView 인스턴스 생성
+        outputView = new OutputView();
+
+        // Bridge 상태를 초기화
+        userBridge = new Bridge();
+    }
+
+    @Test
+    @DisplayName("printMap 메서드 출력 테스트")
+    public void testPrintMap() {
+        Map<Integer, StringBuilder> statusMap = userBridge.getBridgeStatus();
+        statusMap.get(1).append(" O ");
+        statusMap.get(0).append("   ");
+        outputView.printMap(userBridge);
+
+        // 기대하는 출력
+        String expectedOutput = "[ O ]\n[   ]\n";
+
+        // 캡처된 출력 확인
+        assertEquals(expectedOutput, outputStreamCaptor.toString());
+    }
+
+    @Test
+    @DisplayName("printResult 메서드 출력 테스트")
+    public void testPrintResult() {
+        Map<Integer, StringBuilder> statusMap = userBridge.getBridgeStatus();
+        statusMap.get(1).append(" O ");
+        statusMap.get(0).append("   ");
+        outputView.printResult(userBridge);
+
+        // 기대하는 출력
+        String expectedOutput = "최종 게임 결과\n[ O ]\n[   ]\n";
+
+        // 캡처된 출력 확인
+        assertEquals(expectedOutput, outputStreamCaptor.toString());
+    }
 }
