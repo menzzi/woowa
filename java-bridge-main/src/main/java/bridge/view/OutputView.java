69bf0eaae545097bac8664fd47dfd9aab36f955a
menzzi <minmin3927@naver.com>
2024-09-14 15:54:06 +0900

    refactor(view) : OutputView 수정

--- java-bridge-main/src/main/java/bridge/view/OutputView.java
+++ java-bridge-main/src/main/java/bridge/view/OutputView.java
@@ -19,6 +19,8 @@ public class OutputView {
         Map<Integer, StringBuilder> bridgeStatus = userBridge.getBridgeStatus();
         System.out.println("[" + bridgeStatus.get(1).toString() + "]");
         System.out.println("[" + bridgeStatus.get(0).toString() + "]");
+        System.out.println();
+
     }
 
     /**
@@ -27,10 +29,8 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(Bridge userBridge, Result result, int count) {
-        Map<Integer, StringBuilder> bridgeStatus = userBridge.getBridgeStatus();
         System.out.println("최종 게임 결과");
-        System.out.println("[" + bridgeStatus.get(1).toString() + "]");
-        System.out.println("[" + bridgeStatus.get(0).toString() + "]");
+        printMap(userBridge);
         System.out.println();
         System.out.println("게임 성공 여부: " + result.getMessage());
         System.out.println("총 시도한 횟수: " + count);
