f663de32bb49368e479254f4e87e08e82c9a855f
menzzi <minmin3927@naver.com>
2024-09-15 15:51:40 +0900

    feat : InputView 구현

--- java-baseball-6-main/src/main/java/baseball/view/InputView.java
+++ java-baseball-6-main/src/main/java/baseball/view/InputView.java
@@ -1,4 +1,17 @@
 package baseball.view;
 
+import baseball.validator.Validator;
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public static String userNumberInput(){
+        String input = Console.readLine();
+        Validator.validateUserInput(input);
+        return input;
+    }
+    public static String retryInput(){
+        String input = Console.readLine();
+        Validator.validateRetryInput(input);
+        return input;
+    }
 }
