ea70c3f971f30228b42fdf16bc9d0e6b8f94cd0f
menzzi <minmin3927@naver.com>
2024-09-16 13:58:31 +0900

    test : InputView 테스트 구현

---
+++ java-baseball-6-main/src/test/java/baseball/InputViewTest.java
@@ -0,0 +1,57 @@
+package baseball;
+
+import baseball.view.InputView;
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class InputViewTest {
+
+    @AfterEach
+    void closeConsole() {
+        Console.close();
+    }
+
+    @Test
+    void 사용자_숫자입력_실패() {
+        final String input = "21";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputView.userNumberInput();
+        });
+    }
+
+    @Test
+    void 사용자_숫자입력_성공(){
+        final String input = "123";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        assertDoesNotThrow(() -> {
+            InputView.userNumberInput();
+        });
+    }
+    @Test
+    void 재시작_종료_입력_실패(){
+        final String input = "23";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputView.retryInput();
+        });
+    }
+    @Test
+    void 재시작_종료_입력_성공(){
+        final String input = "1";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        assertDoesNotThrow(() -> {
+            InputView.retryInput();
+        });
+    }
+}
