f79e8ddbc2522fc3e7ff1d8ab4d9221b0966f468
menzzi <minmin3927@naver.com>
2024-09-18 15:24:34 +0900

    feat : Validator 구현

---
+++ java-racingcar-6-main/src/main/java/racingcar/validator/Validator.java
@@ -0,0 +1,20 @@
+package racingcar.validator;
+
+import java.util.List;
+
+public class Validator {
+    public static void validateName(List<String> names){
+        for(int i=0;i<names.size();i++){
+            if(names.get(i).length()>6){
+                throw new IllegalArgumentException("이름은 5자 이하입니다. 프로그램을 종료합니다.");
+            }
+        }
+    }
+    public static void validateTryNumber(String number){
+        try{
+            Integer.parseInt(number);
+        }catch(NumberFormatException e){
+            throw new IllegalArgumentException("숫자가 아닙니다. 프로그램을 종료합니다.");
+        }
+    }
+}
