3f7d3357b169ba9005153540f14ec69a588252a7
menzzi <minmin3927@naver.com>
2024-09-18 18:48:49 +0900

    test : InputViewTest 구현

---
+++ java-racingcar-6-main/src/test/java/view/InputViewTest.java
@@ -0,0 +1,39 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import racingcar.view.InputView;
+
+import java.io.ByteArrayInputStream;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class InputViewTest {
+
+    @AfterEach
+    void closeConsole() {
+        Console.close();
+    }
+
+    @Test
+    void 시도횟수_입력_실패() {
+        final String input = "h";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputView.inputTryNumber();
+        });
+    }
+
+    @Test
+    void 시도횟수_입력_성공(){
+        final String input = "3";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        assertDoesNotThrow(() -> {
+            InputView.inputTryNumber();
+        });
+    }
+}
