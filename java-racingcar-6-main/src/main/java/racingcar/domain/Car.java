dc8dec7234072264ae36fb35cc54403161f3226a
menzzi <minmin3927@naver.com>
2024-09-20 19:20:13 +0900

    refactor(domain) : LinkedHashMap으로 변경

--- java-racingcar-6-main/src/main/java/racingcar/domain/Car.java
+++ java-racingcar-6-main/src/main/java/racingcar/domain/Car.java
@@ -1,13 +1,13 @@
 package racingcar.domain;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Car {
 
     public static Map<String,String> resetCar(List<String> names){
-        Map<String,String> cars = new HashMap<>();
+        Map<String,String> cars = new LinkedHashMap<>();
 
         for (String name : names) {
             cars.put(name, "");
