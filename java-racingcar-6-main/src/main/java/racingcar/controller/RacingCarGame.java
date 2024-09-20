df33e0c037a3a06dba47e5741923ee046394906a
menzzi <minmin3927@naver.com>
2024-09-20 18:10:21 +0900

    feat : RacingCarGame 구현 (미완성)

--- java-racingcar-6-main/src/main/java/racingcar/controller/RacingCarGame.java
+++ java-racingcar-6-main/src/main/java/racingcar/controller/RacingCarGame.java
@@ -1,5 +1,6 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.domain.Car;
 import racingcar.validator.Validator;
 import racingcar.view.InputView;
@@ -12,6 +13,7 @@ import java.util.Map;
 public class RacingCarGame {
 
     private final InputView input;
+    private static Map<String,String> cars;
 
     public RacingCarGame(InputView input) {
         this.input = input;
@@ -23,16 +25,34 @@ public class RacingCarGame {
     public void oneGame(){
         List<String> carNames = convertStringToList(input.inputCarNames());
         Validator.validateName(carNames);
-        Map<String,String> cars = Car.resetCar(carNames);
-
+        cars = Car.resetCar(carNames);
+        int tryNum = input.inputTryNumber();
+        proceedRacing(tryNum);
 
     }
     public List<String> convertStringToList(String carNames){
         return Arrays.asList(carNames.split(","));
     }
 
-    public (List<String> carNames){
-
+    public static void proceedRacing(int num){
+        for(int i=0;i<num;i++){
+            moveOnce();
+        }
+    }
+    public static void moveOnce(){
+        for (String carName : cars.keySet()) {
+            if (isFourOrMore()) {
+                plusRoad(carName);
+            }
+        }
+    }
+    public static boolean isFourOrMore(){
+        int randomNum = Randoms.pickNumberInRange(0,9);
+        if(randomNum >= 4) return true;
+        return false;
+    }
+    public static void plusRoad(String carName){
+        cars.put(carName, cars.getOrDefault(carName, "") + "-");
     }
 
 
