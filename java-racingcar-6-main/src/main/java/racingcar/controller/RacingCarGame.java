78b714c98d23e5b96b481b6af83e0ed66a674099
menzzi <minmin3927@naver.com>
2024-09-20 18:17:05 +0900

    feat : RacingCarGame 추가 구현

--- java-racingcar-6-main/src/main/java/racingcar/controller/RacingCarGame.java
+++ java-racingcar-6-main/src/main/java/racingcar/controller/RacingCarGame.java
@@ -5,9 +5,7 @@ import racingcar.domain.Car;
 import racingcar.validator.Validator;
 import racingcar.view.InputView;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 public class RacingCarGame {
@@ -28,6 +26,7 @@ public class RacingCarGame {
         cars = Car.resetCar(carNames);
         int tryNum = input.inputTryNumber();
         proceedRacing(tryNum);
+        List<String> racingResult = compareRoads();
 
     }
     public List<String> convertStringToList(String carNames){
@@ -53,6 +52,20 @@ public class RacingCarGame {
     }
     public static void plusRoad(String carName){
         cars.put(carName, cars.getOrDefault(carName, "") + "-");
+    }
+    public static List<String> compareRoads(){
+        int maxRoad = 0;
+        List<String> result = new ArrayList<>();
+        for (String carName : cars.keySet()) {
+            if(cars.get(carName).length() > maxRoad){
+                maxRoad = cars.get(carName).length();
+                result = new ArrayList<>();
+                result.add(carName);
+            }else if(cars.get(carName).length() == maxRoad){
+                result.add(carName);
+            }
+        }
+        return result;
     }
 
 
