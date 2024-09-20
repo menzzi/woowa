6e7e48aefe04d22f51762f50aa03165b05e2e3e4
menzzi <minmin3927@naver.com>
2024-09-20 17:53:45 +0900

    feat : Car 구현

--- java-racingcar-6-main/src/main/java/racingcar/controller/RacingCarGame.java
+++ java-racingcar-6-main/src/main/java/racingcar/controller/RacingCarGame.java
@@ -1,5 +1,38 @@
 package racingcar.controller;
 
+import racingcar.domain.Car;
+import racingcar.validator.Validator;
+import racingcar.view.InputView;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+
 public class RacingCarGame {
+
+    private final InputView input;
+
+    public RacingCarGame(InputView input) {
+        this.input = input;
+    }
+
+    public void run(){
+
+    }
+    public void oneGame(){
+        List<String> carNames = convertStringToList(input.inputCarNames());
+        Validator.validateName(carNames);
+        Map<String,String> cars = Car.resetCar(carNames);
+
+    }
+    public List<String> convertStringToList(String carNames){
+        return Arrays.asList(carNames.split(","));
+    }
+
+    public (List<String> carNames){
+
+    }
+
 
 }
---
+++ java-racingcar-6-main/src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,24 @@
+package racingcar.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Car {
+
+    public static Map<String,String> plusRoad(List<String> names, Map<String,String> cars){
+        for (String name : names) {
+            cars.put(name, cars.getOrDefault(name, "") + "-");
+        }
+        return cars;
+    }
+
+    public static Map<String,String> resetCar(List<String> names){
+        Map<String,String> cars = new HashMap<>();
+
+        for (String name : names) {
+            cars.put(name, "");
+        }
+        return cars;
+    }
+}
