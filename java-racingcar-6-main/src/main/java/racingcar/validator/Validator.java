746248df677cf46daf28643e61e8b8f02236e436
menzzi <minmin3927@naver.com>
2024-09-21 16:39:00 +0900

    refactor(validator) : validateName 수정

--- java-racingcar-6-main/src/main/java/racingcar/validator/Validator.java
+++ java-racingcar-6-main/src/main/java/racingcar/validator/Validator.java
@@ -7,7 +7,7 @@ public class Validator {
         if(names.isEmpty()) throw new IllegalArgumentException("이름이 입력되지 않았습니다. 프로그램을 종료합니다.");
 
         for(int i=0;i<names.size();i++){
-            if(names.get(i).length()>6){
+            if(names.get(i).length() >= 6){
                 throw new IllegalArgumentException("이름은 5자 이하입니다. 프로그램을 종료합니다.");
             }
         }
