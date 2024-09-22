d0355043e541e1e90b2af70c3a96ebdca3a745c0
menzzi <minmin3927@naver.com>
2024-09-22 15:47:05 +0900

    feat(domain) : validateDuplicate 구현

--- java-lotto-6-main/src/main/java/lotto/domain/Lotto.java
+++ java-lotto-6-main/src/main/java/lotto/domain/Lotto.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -9,12 +10,20 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers){
+        HashSet<Integer> lottoNumbers = new HashSet<>(numbers);
+        if(lottoNumbers.size() != numbers.size()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되면 안됩니다.");
         }
     }
 
