d5a7eff087ffc9f93ca1d866d56fe65ae505a4e4
menzzi <minmin3927@naver.com>
2024-09-22 15:57:21 +0900

    feat(domain) : Result 구현

--- java-lotto-6-main/src/main/java/lotto/domain/Result.java
+++ java-lotto-6-main/src/main/java/lotto/domain/Result.java
@@ -1,4 +1,48 @@
 package lotto.domain;
 
-public class Result {
+public enum Result {
+    FIRST(6,2_000_000_000,"6개 일치 (2,000,000,000원) - "),
+    SECOND(5,30_000_000,"5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD(5,1_500_000,"5개 일치 (1,500,000원) - "),
+    FOURTH(4,50_000,"4개 일치 (50,000원) - "),
+    FIFTH(3,5_000,"3개 일치 (5,000원) - "),
+    MISS(0,0,"");
+
+
+    Result(int countMatch, int lottoMoney, String message) {
+        this.countMatch = countMatch;
+        this.lottoMoney = lottoMoney;
+        this.message = message;
+    }
+
+    private final int countMatch;
+    private final int lottoMoney;
+    private final String message;
+
+    public static Result valueOfMatchCount(int countMatch, boolean hasBonus) {
+        if (countMatch == 6) {
+            return FIRST;
+        }
+        if (countMatch == 5 && hasBonus) {
+            return SECOND;
+        }
+        if (countMatch == 5) {
+            return THIRD;
+        }
+        if (countMatch == 4) {
+            return FOURTH;
+        }
+        if (countMatch == 3) {
+            return FIFTH;
+        }
+        return MISS;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getLottoMoney() {
+        return lottoMoney;
+    }
 }
