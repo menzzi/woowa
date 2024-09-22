ca66e7b07675a493e21b3efc8dd0641e377941d3
menzzi <minmin3927@naver.com>
2024-09-22 16:07:02 +0900

    fest(domain) : bonusNumber 구현

--- java-lotto-6-main/src/main/java/lotto/domain/WinningLotto.java
+++ java-lotto-6-main/src/main/java/lotto/domain/WinningLotto.java
@@ -5,11 +5,14 @@ import java.util.List;
 
 public class WinningLotto {
     private final List<Integer> winningLotto;
+    private final int bonusNumber;
 
-    public WinningLotto(List<Integer> winningLotto){
+    public WinningLotto(List<Integer> winningLotto, int bonusNumber){
         validate(winningLotto);
         validateDuplicate(winningLotto);
+        validateBonusNumber(winningLotto,bonusNumber);
         this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
     }
 
     private void validate(List<Integer> numbers) {
@@ -22,6 +25,12 @@ public class WinningLotto {
         HashSet<Integer> lottoNumbers = new HashSet<>(numbers);
         if(lottoNumbers.size() != numbers.size()){
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되면 안됩니다.");
+        }
+    }
+
+    private void validateBonusNumber(List<Integer> numbers, int bonusNumber){
+        if(numbers.contains(bonusNumber)){
+            throw new IllegalArgumentException("[ERROR] 로또 번호와 중복되면 안됩니다.");
         }
     }
 }
