f62e3ca2a3263b04856912496ed89461d9eb42b3
menzzi <minmin3927@naver.com>
2024-09-22 16:34:34 +0900

    feat(controller) : convertMoneyToTicketCount 구현

--- java-lotto-6-main/src/main/java/lotto/controller/LottoGame.java
+++ java-lotto-6-main/src/main/java/lotto/controller/LottoGame.java
@@ -1,26 +1,29 @@
 package lotto.controller;
 
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoGame {
 
     private final InputView input;
+    private final OutputView output;
 
-    public LottoGame(InputView input) {
+    public LottoGame(InputView input, OutputView output) {
         this.input = input;
+        this.output = output;
     }
     public void run(){
 
     }
     public void proceedGame(){
-        System.out.println("구입금액을 입력해 주세요.");
         String userMoney = input.inputMoney();
         int ticketCount = convertMoneyToTicketCount(userMoney);
+        output.printTicketCount(ticketCount);
 
 
 
     }
     private int convertMoneyToTicketCount(String userMoney){
-
+        return Integer.parseInt(userMoney)/1000;
     }
 }
