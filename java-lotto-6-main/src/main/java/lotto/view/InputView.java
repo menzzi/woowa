c94099d2ecf7793aaac9bf4dda84f49aac099ae8
menzzi <minmin3927@naver.com>
2024-09-22 16:27:52 +0900

    refactor(view) : InputView 수정

--- java-lotto-6-main/src/main/java/lotto/view/InputView.java
+++ java-lotto-6-main/src/main/java/lotto/view/InputView.java
@@ -5,14 +5,17 @@ import lotto.validator.Validator;
 
 public class InputView {
     public static String inputMoney(){
+        System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
         Validator.validateInputMoney(input);
         return input;
     }
     public static String inputWinLottoNumber(){
+        System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
     public static int inputBonusNumber(){
+        System.out.println("보너스 번호를 입력해 주세요.");
         String input = Console.readLine();
         Validator.validateBonusNumber(input);
         return Integer.parseInt(input);
