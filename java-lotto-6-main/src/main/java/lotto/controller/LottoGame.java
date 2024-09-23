3ebdde73481f1909a2c7f12cedd37ca0c7cfa07a
menzzi <minmin3927@naver.com>
2024-09-23 16:31:11 +0900

    feat(controller) : LottoGame 구현(미완성)

--- java-lotto-6-main/src/main/java/lotto/controller/LottoGame.java
+++ java-lotto-6-main/src/main/java/lotto/controller/LottoGame.java
@@ -1,5 +1,7 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
+import lotto.domain.WinningLotto;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -10,10 +12,12 @@ public class LottoGame {
 
     private final InputView input;
     private final OutputView output;
+    private WinningLotto winningLotto;
 
-    public LottoGame(InputView input, OutputView output) {
+    public LottoGame(InputView input, OutputView output, WinningLotto winningLotto) {
         this.input = input;
         this.output = output;
+        this.winningLotto = winningLotto;
     }
 
     public void run(){
@@ -24,7 +28,15 @@ public class LottoGame {
         String userMoney = inputMoney();
         int ticketCount = convertMoneyToTicketCount(userMoney);
         output.printTicketCount(ticketCount);
-
+        List<Lotto> lottos = new ArrayList<>();
+        for(int i=0;i<ticketCount;i++){
+            List<Integer> lotto = Lotto.generateLotto();
+            lottos.add(new Lotto(lotto));
+        }
+        //로또 결과 출력
+        winningLotto = makeWinningLotto();
+        // 생성한 로또와 결과 비교하기
+        // 수익률 출력하기
 
 
     }
@@ -32,10 +44,12 @@ public class LottoGame {
     private int convertMoneyToTicketCount(String userMoney){
         return Integer.parseInt(userMoney)/1000;
     }
-
-    private List<Integer> convertStringToList(String input){
-        String[] splitInput = input.split(",");
-        List<Integer> numbers = new ArrayList<>();
+    private List<Integer> covertStringToList(String[] numbers){
+        List<Integer> WinningLotto = new ArrayList<>();
+        for(String value : numbers){
+            WinningLotto.add(Integer.parseInt(value));
+        }
+        return WinningLotto;
     }
 
     private String inputMoney(){
@@ -46,5 +60,36 @@ public class LottoGame {
             return inputMoney();
         }
         return userMoney;
+    }
+
+    private String[] inputWinningLotto(){
+        String[] lotto;
+        try{
+            lotto = input.inputWinLottoNumber();
+        }catch(IllegalArgumentException e){
+            return inputWinningLotto();
+        }
+        return lotto;
+    }
+
+    private int inputBonusNumber(){
+        int bonus;
+        try{
+            bonus = input.inputBonusNumber();
+        }catch(IllegalArgumentException e){
+            return inputBonusNumber();
+        }
+        return bonus;
+    }
+
+    private WinningLotto makeWinningLotto(){
+        try{
+            String[] winningNumbers = inputWinningLotto();
+            int bonus = inputBonusNumber();
+            winningLotto = new WinningLotto(covertStringToList(winningNumbers),bonus);
+        }catch(IllegalArgumentException e){
+            return makeWinningLotto();
+        }
+        return winningLotto;
     }
 }
