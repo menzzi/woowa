8fa8413c16c8a5e43ab6cdcc5f2d07011e56d7db
menzzi <minmin3927@naver.com>
2024-09-25 16:19:49 +0900

    refactor(domain) : Lotto - generateLotto 오름차순 정렬로 수정

--- java-lotto-6-main/src/main/java/lotto/domain/Lotto.java
+++ java-lotto-6-main/src/main/java/lotto/domain/Lotto.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
@@ -29,7 +30,9 @@ public class Lotto {
     }
 
     public static List<Integer> generateLotto(){
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(lotto);
+        return lotto;
     }
 
     public int countMatchNumber(WinningLotto winningLotto){
