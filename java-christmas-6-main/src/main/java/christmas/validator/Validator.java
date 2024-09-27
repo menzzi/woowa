f7895df19a1c890a1957fed3b05db1c810a516ea
menzzi <minmin3927@naver.com>
2024-09-27 13:44:33 +0900

    style : validator 수정

--- java-christmas-6-main/src/main/java/christmas/validator/Validator.java
+++ java-christmas-6-main/src/main/java/christmas/validator/Validator.java
@@ -5,6 +5,7 @@ public class Validator {
         validateNumberFormat(input);
         validateDateRange(input);
     }
+
     public static void validateNumberFormat(String input){
         try{
             Integer.parseInt(input);
@@ -12,6 +13,7 @@ public class Validator {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
+
     public static void validateDateRange(String input){
         int date = Integer.parseInt(input);
         if(date < 1 || date > 31){
