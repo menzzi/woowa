dbb1ab5b249c545ac8d502d00d8bc6b61424d205
menzzi <minmin3927@naver.com>
2024-09-27 18:09:07 +0900

    feat(view) : inputMenu 메서드 구현

--- java-christmas-6-main/src/main/java/christmas/view/InputView.java
+++ java-christmas-6-main/src/main/java/christmas/view/InputView.java
@@ -10,4 +10,10 @@ public class InputView {
         Validator.validateExpectedDate(input);
         return input;
     }
+
+    public static String inputMenu(){
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String input = Console.readLine();
+        return input;
+    }
 }
