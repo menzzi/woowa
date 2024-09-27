0f56c581f0bd008b3a2139d67d449d94ca8ad3c3
menzzi <minmin3927@naver.com>
2024-09-27 13:41:59 +0900

    feat(validator) : validateExpectedDate 메서드 구현

---
+++ java-christmas-6-main/src/main/java/christmas/validator/Validator.java
@@ -0,0 +1,21 @@
+package christmas.validator;
+
+public class Validator {
+    public static void validateExpectedDate(String input){
+        validateNumberFormat(input);
+        validateDateRange(input);
+    }
+    public static void validateNumberFormat(String input){
+        try{
+            Integer.parseInt(input);
+        }catch(NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+    public static void validateDateRange(String input){
+        int date = Integer.parseInt(input);
+        if(date < 1 || date > 31){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+}
