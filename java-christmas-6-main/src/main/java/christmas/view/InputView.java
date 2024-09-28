77e5208511641b16e45c6d8bbf77ecee78670a2a
menzzi <minmin3927@naver.com>
2024-09-28 13:15:32 +0900

    refactor(view) : InputView 수정

--- java-christmas-6-main/src/main/java/christmas/view/InputView.java
+++ java-christmas-6-main/src/main/java/christmas/view/InputView.java
@@ -4,14 +4,14 @@ import camp.nextstep.edu.missionutils.Console;
 import christmas.validator.Validator;
 
 public class InputView {
-    public static String inputExpectedDate(){
+    public String inputExpectedDate(){
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = Console.readLine();
         Validator.validateExpectedDate(input);
         return input;
     }
 
-    public static String inputMenu(){
+    public String inputMenu(){
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         String input = Console.readLine();
         return input;
