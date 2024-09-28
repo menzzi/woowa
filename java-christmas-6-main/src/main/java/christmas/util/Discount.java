914cb48d84a6dc8f80d3873a658699b7976d329d
menzzi <minmin3927@naver.com>
2024-09-28 12:54:45 +0900

    feat(util) : Discount - weekdayDiscount 구현

--- java-christmas-6-main/src/main/java/christmas/util/Discount.java
+++ java-christmas-6-main/src/main/java/christmas/util/Discount.java
@@ -5,6 +5,7 @@ import java.util.Set;
 public class Discount {
     private static final Set<Integer> WEEKEND_DATES = Set.of(1,2,8,9,15,16,22,23,29,30);
     private static final Set<Integer> STAR_DATES = Set.of(3,10,17,24,25,31);
+    private static final int WEEKDAY_DISCOUNT_AMOUNT = 2023;
     private static final int WEEKEND_DISCOUNT_AMOUNT = 2023;
     private static final int SPECIAL_DISCOUNT_AMOUNT = 1000;
     private static final int EVENT_START = 1;
@@ -15,6 +16,13 @@ public class Discount {
         if(orderDate > CHRISTMAS_EVENT_END){
             return 0;
         }
-        return 1000 + 100 * (orderDate - 1);
+        return 1000 + 100 * (orderDate - EVENT_START);
+    }
+
+    public int weekdayDiscount(int orderDate, int dessertCount){
+        if(STAR_DATES.contains(orderDate) || WEEKEND_DATES.contains(orderDate)){
+            return 0;
+        }
+        return dessertCount * WEEKDAY_DISCOUNT_AMOUNT;
     }
 }
