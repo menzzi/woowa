f4b09b717332b366685cdae2a060c360c3b5661f
menzzi <minmin3927@naver.com>
2024-09-28 15:13:30 +0900

    refactor(view) : OutputView - printOrderMenu 수정

--- java-christmas-6-main/src/main/java/christmas/view/OutputView.java
+++ java-christmas-6-main/src/main/java/christmas/view/OutputView.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 public class OutputView {
     public void printOrderMenu(Map<String,Integer> orderList){
+        System.out.println("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        System.out.println();
         System.out.println("<주문 메뉴>");
         for(Map.Entry<String,Integer> order: orderList.entrySet()){
             System.out.println(order.getKey() + " " + order.getValue() + "개");
