0d4fd9ae5017f4d3dace84e74d8f2252f21d2bf6
menzzi <minmin3927@naver.com>
2024-09-30 16:57:40 +0900

    refactor(domain) : MenuCategory 카테고리이름 변수 삭제

--- java-christmas-6-main/src/main/java/christmas/domain/MenuCategory.java
+++ java-christmas-6-main/src/main/java/christmas/domain/MenuCategory.java
@@ -3,23 +3,17 @@ package christmas.domain;
 import java.util.List;
 
 public class MenuCategory {
-    private String categoryName;
     private List<MenuItem> items;
 
     public MenuCategory(String categoryName, List<MenuItem> items){
-        this.categoryName = categoryName;
         this.items = items;
     }
-    public MenuItem getItemByName(String name) throws IllegalArgumentException {
+    public MenuItem getItemByName(String categoryName) throws IllegalArgumentException {
         for (MenuItem item : items) {
-            if (item.getName().equalsIgnoreCase(name)) {
+            if (item.getName().equalsIgnoreCase(categoryName)) {
                 return item;
             }
         }
         throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-    }
-
-    public String getCategoryName() {
-        return categoryName;
     }
 }
